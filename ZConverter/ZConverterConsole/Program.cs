e120aafa3fe621aaecab5e02e0c67692def7cab1
snoy77 <snoy77@mail.ru>
2026-03-22 15:38:47 +0300

    start develop

---
+++ ZConverter/ZConverterConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ZConverterConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
